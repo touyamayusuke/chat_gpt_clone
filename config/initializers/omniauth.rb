2f3098430228956ce79062f6a4ef8a1530acbb78
touyamayusuke <yusuke2710@icloud.com>
2026-03-24 04:55:27 +0000

    Fix omniauth nil credentials guard and configure remote builder

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,13 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Rails.application.credentials.google.client_id, 
-            Rails.application.credentials.google.client_secret, 
-            {
-              scope: 'email,profile',
-              prompt: 'select_account',
-              image_aspect_ratio: 'square',
-              image_size: 50
-            }
+  if (google_creds = Rails.application.credentials.google)
+    provider :google_oauth2, google_creds.client_id,
+              google_creds.client_secret,
+              {
+                scope: 'email,profile',
+                prompt: 'select_account',
+                image_aspect_ratio: 'square',
+                image_size: 50
+              }
+  end
 end
 OmniAuth.config.allowed_request_methods = %i[get]
